29446a21b2ff549c8cad9db33e8f5df7d124e96b
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-18 16:42:20 -0300

    created mocks for tests

---
+++ src/services/getCityByNameService.test.ts
@@ -0,0 +1,12 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/mockCityAPIResponse";
+import { api } from "./api";
+import { getCityByNameService } from "./getCityByNameService"
+
+describe("Service: getCityByNameService", () => {
+    it("should return city details", async () => {
+        jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse }); 
+        // we let's listen the requests to the api with method 'get' - let's created a mock of return this api
+        const response = await getCityByNameService("São Paulo");
+        expect(response.length).toBeGreaterThan(0);
+    });
+});
