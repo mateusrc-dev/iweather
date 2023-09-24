5786e1af81436bc89812836836837696d087dd32
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-24 13:10:51 -0300

    updated project

--- src/contexts/CityContext.tsx
+++ src/contexts/CityContext.tsx
@@ -1,19 +1,25 @@
-import { ReactNode, createContext, useEffect, useState, } from 'react';
+import { ReactNode, createContext, useEffect, useState } from "react";
 
-import { CityProps } from '@services/getCityByNameService';
-import { getStorageCity, removeStorageCity, saveStorageCity } from '@libs/asyncStorage/cityStorage';
+import { CityProps } from "@services/getCityByNameService";
+import {
+  getStorageCity,
+  removeStorageCity,
+  saveStorageCity,
+} from "@libs/asyncStorage/cityStorage";
 
 type CityContextProviderProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 type CityContextDataProps = {
   cityIsLoading: boolean;
   city: CityProps | null;
   handleChanceCity: (city: CityProps) => void;
-}
+};
 
-export const CityContext = createContext<CityContextDataProps>({} as CityContextDataProps);
+export const CityContext = createContext<CityContextDataProps>(
+  {} as CityContextDataProps
+);
 
 export function CityProvider({ children }: CityContextProviderProps) {
   const [cityIsLoading, setCityIsLoading] = useState(true);
@@ -31,18 +37,22 @@ export function CityProvider({ children }: CityContextProviderProps) {
   useEffect(() => {
     setCityIsLoading(true);
 
+    //removeStorageCity();
+
     getStorageCity()
       .then((data) => setCity(data))
       .finally(() => setCityIsLoading(false));
   }, []);
 
   return (
-    <CityContext.Provider value={{
-      city,
-      cityIsLoading,
-      handleChanceCity
-    }}>
+    <CityContext.Provider
+      value={{
+        city,
+        cityIsLoading,
+        handleChanceCity,
+      }}
+    >
       {children}
     </CityContext.Provider>
-  )
+  );
 }
